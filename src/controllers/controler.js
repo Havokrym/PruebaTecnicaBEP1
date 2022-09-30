7b12c6d245f42a985604912999018387ffa01e13
enrique sanchez <esanchez1806@alumno.ipn.mx>
2022-09-30 09:44:48 -0500

    Commit Final

--- src/controllers/controler.js
+++ src/controllers/controler.js
@@ -16,9 +16,35 @@ controler.raiz = async(req, res) => {
     )
     res.json({id:insertar._id});
     }
+
     controler.get= async(req, res) =>{
         let id_objeto = req.query.id
         const objeto = await accidentesModel.find({_id:id_objeto})
         res.json({objeto}) 
     }
+
+    controler.put= async(req, res)=>{
+        let query_id = req.query.id
+        let tipo_de_vehiculo_1 = req.query.tv1;
+        let tipo_de_vehiculo_2 = req.query.tv2;
+        let tipo_de_vehiculo_3 = req.query.tv3;
+        let tipo_de_vehiculo_4 = req.query.tv4;
+
+        const actual = await accidentesModel.updateOne({_id:query_id}, {
+            tipo_de_vehiculo_1:tipo_de_vehiculo_1,
+            tipo_de_vehiculo_2: tipo_de_vehiculo_2,
+            tipo_de_vehiculo_3: tipo_de_vehiculo_3,
+            tipo_de_vehiculo_4: tipo_de_vehiculo_4,
+        })
+
+        res.json({actual});
+    }
+
+    controler.delete= async(req,res)=>{
+        let id = req.query.id
+        const borra = await accidentesModel.deleteOne({_id:id})
+        res.json({borra});
+    }
+
+
 module.exports = controler;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,8 +3,12 @@ const {Router} = require("express");
 const router = Router();
 const controlar = require('../controllers/controler')
 
-router.post('/', controlar.raiz);
+router.post('/1', controlar.raiz);
 
-router.get('/getobjeto', controlar.get)
+router.get('/2', controlar.get)
+
+router.put('/3', controlar.put)
+
+router.delete('/4', controlar.delete)
 
 module.exports = router;  
